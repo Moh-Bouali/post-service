97586c6ec894f111f47a3d7116e63b5861e7b83e
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-12-09 18:49:45 +0100

    added-rest-template

--- post-service/src/main/java/com/individual_s7/post_service/service/PostService.java
+++ post-service/src/main/java/com/individual_s7/post_service/service/PostService.java
@@ -13,18 +13,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
